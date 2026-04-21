3d761be9350c18912ef9e5fa4069836c37a2c725
POOJANAIK17 <poohnk1798@gmail.com>
2026-04-21 21:13:13 +0530

    logo change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,8 +58,8 @@ const Navbar = () => {
             className={`transition-all duration-500
               ${
                 scrolled
-                  ? "h-12 scale-100 brightness-100"
-                  : "h-16 scale-[1.05] brightness-180 drop-shadow-[0_8px_22px_rgba(0,0,0,0.4)]"
+                  ? "h-12 scale-100"
+                  : "h-16 scale-[1.05] drop-shadow-[0_8px_22px_rgba(0,0,0,0.4)]"
               }
             `}
           />
